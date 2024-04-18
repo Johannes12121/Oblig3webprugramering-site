01ed7de80b9a0f560d44639e452030bb9625c6fc
Johannes12121 <josae4857@oslomet.no>
2024-04-18 12:52:27 +0200

    test1

---
+++ src/main/java/oslomettest1/oblig3final_webpruug/Oblig3FinalWebpruugApplication.java
@@ -0,0 +1,13 @@
+package oslomettest1.oblig3final_webpruug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3FinalWebpruugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig3FinalWebpruugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oslomettest1/oblig3final_webpruug/Oblig3FinalWebpruugApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomettest1.oblig3final_webpruug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3FinalWebpruugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
